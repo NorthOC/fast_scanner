5be0d4bb8e858199a188d1b8071b9de999710653
NorthOC <rev.denisas@gmail.com>
2023-03-28 16:47:50 +0300

    eye-candy

--- main.py
+++ main.py
@@ -2,18 +2,6 @@ import json
 import requests
 from datetime import datetime as date
 
-#APIs
-
-# search all URL: COUNTRY - ANYWHERE (ANYTIME)
-# https://www.skyscanner.net/g/browse-view-bff/dataservices/browse/v3/bvweb/UK/EUR/en-GB/destinations/{COUNTRY}/anywhere/anytime/anytime/?profile=minimalcityrollupwithnamesv2&include=image;hotel&apikey=8aa374f4e28e4664bf268f850f767535&isMobilePhone=false
-
-# after selecting specific country: COUNTRY - DESTINATION (ANYTIME)
-# https://www.skyscanner.net/g/browse-view-bff/dataservices/browse/v3/bvweb/UK/EUR/en-GB/destinations/{COUNTRY}/{destination}/anytime/anytime/?profile=minimalcityrollupwithnamesv2&include=image;hotel&apikey=8aa374f4e28e4664bf268f850f767535&isMobilePhone=false
-
-# Load specific country airports
-# https://www.skyscanner.net/g/browse-view-bff/dataservices/browse/v3/bvweb/UK/EUR/en-GB/destinations/{COUNTRY}/{DEST_COUNTRY}/anytime/anytime/?profile=minimalcityrollupwithnamesv2&include=image;hotel&apikey=8aa374f4e28e4664bf268f850f767535&isMobilePhone=false
-
-
 class Skyscanner:
     COUNTRY_CODE_API = 'https://gist.githubusercontent.com/anubhavshrimal/75f6183458db8c453306f93521e93d37/raw/f77e7598a8503f1f70528ae1cbf9f66755698a16/CountryCodes.json'
     HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'}
@@ -34,34 +22,36 @@ class Skyscanner:
         self.select_to_country()
         self.select_to_airport()
         self.select_from_airport()
-        self.output_flight_dates()
+        self.select_flight_dates()
         self.output_flight_url()
 
     def select_from_country(self):
-        # Select a country code from an API
+        # Request country codes
         res = requests.get(Skyscanner.COUNTRY_CODE_API, headers=Skyscanner.HEADERS)
         countries = json.loads(res.text)
         while True:
-            search = str(input("Enter your country code: ")).strip().upper()
+            search = str(input("Enter your country code or country name: ")).strip().lower()
             for country in countries:
-                if country["code"] == search:
+                if country["code"].lower() == search or country["name"].lower() == search:
+                    # add country object as class instance variable
                     self.from_country = country
+                    print(self.from_country)
                     return 0
             print("Country code not found. Try again.")
 
     def select_to_country(self):
-        print(self.from_country)
-        from_country_code = self.from_country['code'].upper()
+        from_country_code = self.from_country['code']
         #skyscanner API
-        api_url = f'https://www.skyscanner.net/g/browse-view-bff/dataservices/browse/v3/bvweb/UK/EUR/en-GB/destinations/{from_country_code}/anywhere/anytime/anytime/?profile=minimalcityrollupwithnamesv2&include=image;hotel;adverts&apikey=8aa374f4e28e4664bf268f850f767535&isMobilePhone=false&isOptedInForPersonalised=false'
+        api_url = f'https://www.skyscanner.net/g/browse-view-bff/dataservices/browse/v3/bvweb/UK/EUR/en-GB/destinations/{from_country_code}/anywhere/anytime/anytime/?apikey=8aa374f4e28e4664bf268f850f767535'
         res = requests.get(api_url, headers=Skyscanner.HEADERS)
-
         data = self.parse_skyscanner_api(json.loads(res.text))
 
         self.print_api_results(data)
 
+        country_name = self.from_country["name"]
+
         while True:
-            selected_id = int(input(f"Select country you want to fly to (enter number): ")) - 1
+            selected_id = int(input(f"Select country you want to fly to from {country_name} (enter number): ")) - 1
             if selected_id in range(0,len(data["PlacePrices"])):
                 break
         data["PlacePrices"].reverse()
@@ -71,12 +61,13 @@ class Skyscanner:
         return 0
     
     def parse_skyscanner_api(self, data):
-
+        # All objects are stored in PlacePrices in API
         data["PlacePrices"].sort(key=self.sort_func, reverse=True)
-
         for i in data["PlacePrices"].copy():
+            # In case there is no price
             if "DirectPrice" not in i and "IndirectPrice" not in i:
                 price = 0
+            # In case there are both direct and indirect prices, pick the cheaper one
             elif "DirectPrice" in i and "IndirectPrice" in i:
                 price_direct = i['DirectPrice']
                 price_indirect = i["IndirectPrice"]
@@ -88,7 +79,7 @@ class Skyscanner:
                 price = i["IndirectPrice"]
             elif "IndirectPrice" not in i:
                 price = i['DirectPrice']
-                
+            # remove country if price is 0
             if price == 0:
                 data["PlacePrices"].remove(i)
         return data
@@ -135,8 +126,10 @@ class Skyscanner:
 
         self.print_api_results(data)
 
+        country_name = self.to_country["Name"]
+
         while True:
-            selected_id = int(input(f"Select an airport you want to fly to (enter number): ")) - 1
+            selected_id = int(input(f"Select an airport in {country_name} you want to fly to (enter number): ")) - 1
             if selected_id in range(0,len(data["PlacePrices"])):
                 break
         data["PlacePrices"].reverse()
@@ -156,8 +149,10 @@ class Skyscanner:
 
         self.print_api_results(data)
 
+        country_name = self.from_country['name']
+
         while True:
-            selected_id = int(input(f"Select an airport you want to fly from (enter number): ")) - 1
+            selected_id = int(input(f"Select an airport in {country_name} you want to fly from (enter number): ")) - 1
             if selected_id in range(0,len(data["PlacePrices"])):
                 break
         data["PlacePrices"].reverse()
@@ -166,7 +161,7 @@ class Skyscanner:
         self.from_airport = data["PlacePrices"][selected_id]
         return 0
     
-    def output_flight_dates(self):
+    def select_flight_dates(self):
         from_airport_code = self.from_airport["Id"]
         to_airport_code = self.to_airport["Id"]
         api_url = f'https://www.skyscanner.net/g/monthviewservice/LT/EUR/en-GB/calendar/{from_airport_code}/{to_airport_code}/cheapest/cheapest/?abvariant=rts_who_precompute:a&apikey=6f4cb8367f544db99cd1e2ea86fb2627'
@@ -211,7 +206,7 @@ class Skyscanner:
 
         while True:
             try:
-                selected_id = int(input(f"Select your flight (enter a number): ")) - 1
+                selected_id = int(input(f"Select your flight date (enter a number): ")) - 1
             except ValueError:
                 continue
             if selected_id in range(0,len(found_flights)):
@@ -248,6 +243,7 @@ class Skyscanner:
     
     def flight_sort(self, e):
         return e['price']
+    
         
 scanner = Skyscanner()
 scanner.scan()
